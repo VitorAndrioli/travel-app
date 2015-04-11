a36f9c650f761fa972b0832852899efe38827485
Vitor Andrioli <vtr_andrioli@hotmail.com>
2015-04-11 12:55:10 -0300

    Começo da captura de imagens.

--- app/src/main/java/com/traveltrack/vitor/travelapp/LoginActivity.java
+++ app/src/main/java/com/traveltrack/vitor/travelapp/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.traveltrack.vitor.travelapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class LoginActivity extends Activity {
@@ -13,6 +15,12 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void enter(View view) {
+        Intent i = new Intent(LoginActivity.this, TravelsActivity.class);
+        startActivity(i);
+        finish();
     }
 
 
--- app/src/main/java/com/traveltrack/vitor/travelapp/TravelActivity.java
+++ app/src/main/java/com/traveltrack/vitor/travelapp/TravelActivity.java
@@ -1,12 +1,16 @@
 package com.traveltrack.vitor.travelapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TravelActivity extends ActionBarActivity {
+public class TravelActivity extends Activity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +18,10 @@ public class TravelActivity extends ActionBarActivity {
         setContentView(R.layout.activity_travel);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_travel, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
